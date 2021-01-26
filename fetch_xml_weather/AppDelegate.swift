5fbbf20bfc170f3683784315b156eb3f831b2d7d
shinohara-san <someiyoshino0809@gmail.com>
2021-01-26 12:45:07 +0900

    background tasks probably done in SceneDelegate

--- fetch_xml_weather/AppDelegate.swift
+++ fetch_xml_weather/AppDelegate.swift
@@ -8,77 +8,66 @@
 import UIKit
 import BackgroundTasks
 
-class RefreshAppContentsOperation: Operation {
-    let id: Int
-    
-    init(id: Int) {
-        self.id = id
-    }
-    
-    override func main() {
-        print("this operation id is \(self.id)")
-    }
-}
-
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    let taskIdentifier = "com.fetch.xml.weather.refresh"
+class AppDelegate: UIResponder, UIApplicationDelegate, UIWindowSceneDelegate {
+//    確認
+//    e -l objc -- (void)[[BGTaskScheduler sharedScheduler] _simulateLaunchForTaskWithIdentifier:@"com.fetch.xml.weather.refresh"]
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        registerBackgroundTaks()
+        print("didFinishLaunchingWithOptions")
+//        registerTask()
         return true
     }
     
-    //MARK: Register BackGround Tasks
-    private func registerBackgroundTaks() {
-        
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.SO.imagefetcher", using: nil) { task in
-            //This task is cast with processing request (BGProcessingTask)
-            self.scheduleLocalNotification()
-            self.handleImageFetcherTask(task: task as! BGProcessingTask)
-        }
-        
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.SO.apprefresh", using: nil) { task in
-            //This task is cast with processing request (BGAppRefreshTask)
-            self.scheduleLocalNotification()
-            self.handleAppRefreshTask(task: task as! BGAppRefreshTask)
-        }
-    }
-    
     func applicationDidEnterBackground(_ application: UIApplication) {
-        scheduleAppRefresh()
-        scheduleImagefetcher()
+        print("applicationDidEnterBackground")
+//        cancelAllPandingBGTask()
+//        scheduleAppRefresh()
     }
     
-    func scheduleImagefetcher() {
-        let request = BGProcessingTaskRequest(identifier: "com.SO.imagefetcher")
-        request.requiresNetworkConnectivity = false // Need to true if your task need to network process. Defaults to false.
-        request.requiresExternalPower = false
-        //If we keep requiredExternalPower = true then it required device is connected to external power.
-        
-        request.earliestBeginDate = Date(timeIntervalSinceNow: 1 * 60) // fetch Image Count after 1 minute.
-        //Note :: EarliestBeginDate should not be set to too far into the future.
-        do {
-            try BGTaskScheduler.shared.submit(request)
-        } catch {
-            print("Could not schedule image fetch: \(error)")
-        }
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        print("sceneDidEnterBackground")
+//        cancelAllPandingBGTask()
+//        scheduleAppRefresh()
     }
+    
+    func registerTask(){
+        print("registerTask")
+//        BGTaskScheduler.shared.register(forTaskWithIdentifier: taskIdentifier, using: nil) { task in
+//            print("bgtask登録されたよ")//この実行は登録だけなので起動時にはプリントされない→よって、起動時はregisterTaskのみプリントされる
+//            self.handleRefresh(task: task as! BGAppRefreshTask)
+//        }
+    }
+    
+    
     
     func scheduleAppRefresh() {
-        let request = BGAppRefreshTaskRequest(identifier: "com.SO.apprefresh")
-        request.earliestBeginDate = Date(timeIntervalSinceNow: 2 * 60) // App Refresh after 2 minute.
-        //Note :: EarliestBeginDate should not be set to too far into the future.
-        do {
-            try BGTaskScheduler.shared.submit(request)
-        } catch {
-            print("Could not schedule app refresh: \(error)")
-        }
+        print("scheduleAppRefresh")
+//        let request = BGAppRefreshTaskRequest(identifier: taskIdentifier)
+//        request.earliestBeginDate = Date(timeIntervalSinceNow: 2 * 60) // App Refresh after 2 minute.
+//        //Note :: EarliestBeginDate should not be set to too far into the future.
+//        do {
+//            try BGTaskScheduler.shared.submit(request)
+//        } catch {
+//            print("Could not schedule app refresh: \(error)")
+//        }
     }
     
-    func cancelAllPendingBGTask() {
-        BGTaskScheduler.shared.cancelAllTaskRequests()
+    func handleRefresh(task: BGAppRefreshTask){
+        
+//        print("handleAppRefreshTask")
+//        task.expirationHandler = {
+//            //This Block call by System
+//            //Canle your all tak's & queues
+//        }
+////        scheduleLocalNotification()
+//        //
+//        task.setTaskCompleted(success: true)
+    }
+    
+    func cancelAllPandingBGTask() {
+        print("cancelAllPandingBGTask")
+//        BGTaskScheduler.shared.cancelAllTaskRequests()
     }
     
     // MARK: UISceneSession Lifecycle
--- fetch_xml_weather/SceneDelegate.swift
+++ fetch_xml_weather/SceneDelegate.swift
@@ -6,17 +6,18 @@
 //
 
 import UIKit
+import BackgroundTasks
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        registerTask()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -24,24 +25,33 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        print("さよなら!")
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        print("アクティブ!")
+        
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        print("resignアクティブ!")
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        print("bgに入るよ!")
+        
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
+        print("bg!")
+        cancelAllPandingBGTask()
+        scheduleAppRefresh()
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
@@ -50,3 +60,44 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+extension SceneDelegate {
+    func registerTask(){
+        print("registerTask")
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.fetch.xml.weather.refresh", using: nil) { task in
+            // コンテンツの取得などを行う
+            print("bgtask登録されたよ")//この実行は登録だけなので起動時にはプリントされない→よって、起動時はregisterTaskのみプリントされる
+            self.handleRefresh(task: task as! BGAppRefreshTask)
+        }
+    }
+    
+    
+    
+    func scheduleAppRefresh() {
+        print("scheduleAppRefresh")
+        let request = BGAppRefreshTaskRequest(identifier: "com.fetch.xml.weather.refresh")
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 2 * 60) // App Refresh after 2 minute.
+        //Note :: EarliestBeginDate should not be set to too far into the future.
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch {
+            print("Could not schedule app refresh: \(error)")
+        }
+    }
+    
+    func handleRefresh(task: BGAppRefreshTask){
+        
+        print("handleAppRefreshTask")
+        task.expirationHandler = {
+            //This Block call by System
+            //Canle your all tak's & queues
+        }
+//        scheduleLocalNotification()
+        //
+        task.setTaskCompleted(success: true)
+    }
+    
+    func cancelAllPandingBGTask() {
+        print("cancelAllPandingBGTask")
+        BGTaskScheduler.shared.cancelAllTaskRequests()
+    }
+}
